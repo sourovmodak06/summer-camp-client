22e180b6d084ea6b3a4da3e67aed818c264711ec
sourovmodak06 <sourovmodak650@gmail.com>
2023-06-10 18:17:56 +0600

    Student Dashboard update delete oparation added

---
+++ src/components/studentClassCard/StudentClassCard.jsx
@@ -0,0 +1,70 @@
+import { FaCcMastercard, FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useClasses from "../../hooks/useClasses";
+const StudentClassCard = ({ item, index }) => {
+  const [, refetch] = useClasses();
+  const { image, name, instructorName, price, enrolledStudent } = item;
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(
+          `https://school-of-rock-server.vercel.app/classCart/${item._id}`,
+          {
+            method: "DELETE",
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+  return (
+    <tr>
+      <td className="text-center">{index + 1}</td>
+      <td>
+        <img
+          src={image}
+          alt="img"
+          className="w-14 h-14 my-3 mx-5 rounded-full "
+        />
+      </td>
+      <td className="text-center">
+        <h2>{name}</h2>
+      </td>
+      <td className="text-center">
+        <h2>{instructorName}</h2>
+      </td>
+      <td className="text-center">
+        <h2>{price}</h2>
+      </td>
+      <td className="text-center">
+        <h2>{enrolledStudent}</h2>
+      </td>
+      <td className="text-center" onClick={() => handleDelete(item)}>
+        <button className="text-2xl text-[#D31A50]">
+          <FaTrash></FaTrash>
+        </button>
+      </td>
+      <td className="text-center">
+        <button className=" text-[#03203C] text-2xl">
+          <FaCcMastercard></FaCcMastercard>
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default StudentClassCard;
--- src/pages/dashboard/studentClasses/StudentClasses.jsx
+++ src/pages/dashboard/studentClasses/StudentClasses.jsx
@@ -1,13 +1,45 @@
+import StudentClassCard from "../../../components/studentClassCard/StudentClassCard";
+import useClasses from "../../../hooks/useClasses";
 import useTitle from "../../../hooks/useTitle";
 
-
 const StudentClasses = () => {
-    useTitle("Dashboard | Selected");
-    return (
-        <div>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem ipsam vel atque quo suscipit nisi repellendus modi vero delectus.</h2>
+  useTitle("Dashboard | Selected");
+  const [classItems] = useClasses();
+  console.log("class", classItems);
+  const total = classItems.reduce((sum, item) => item.price + sum, 0);
+  return (
+    <div>
+      <div className="bg-[#ffffff4d] shadow-2xl drop-shadow-2xl rounded-2xl h-[95vh] p-6 overflow-scroll">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl text-white">
+            Selected Classes: {classItems.length}
+          </h2>
+          <h2 className="text-2xl text-white">Total Price : ${total}</h2>
         </div>
-    );
+        <div className="w-full">
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Images</th>
+                <th>Name Of Class</th>
+                <th>Instructor Name</th>
+                <th>Price</th>
+                <th>Already Enrolled</th>
+                <th>Action</th>
+                <th>Pay</th>
+              </tr>
+            </thead>
+            <tbody className="">
+              {classItems.map((item, index) => (
+                <StudentClassCard key={item._id} item={item} index={index}></StudentClassCard>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StudentClasses;
